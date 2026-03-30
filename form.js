39a2149534ab488b56b063c86cd3f3b346258c1e
Linda Winther <Linda.Winther@outlook.com>
2026-03-30 15:56:47 +0200

    Typ klar med valideringen. behöver lite mer fix med utseendet
    Co-authored-by: walterfilip

--- form.js
+++ form.js
@@ -3,8 +3,22 @@
 const form = document.getElementById("order");
 const nameInput = document.getElementById("name");
 const nameError = document.getElementById("nameError");
+
 const emailInput = document.getElementById("email");
 const emailError = document.getElementById("emailError");
+
+const numberInput = document.getElementById("phonenumber");
+const numberError = document.getElementById("numberError");
+
+const streetInput = document.getElementById("streetName");
+const streetError = document.getElementById("streetError");
+
+const postnumberInput = document.getElementById("postnumber");
+const postnumberError = document.getElementById("postnumberError");
+
+const cityInput = document.getElementById("city");
+const cityError = document.getElementById("cityError");
+
 const result = document.getElementById("result")
 
 
@@ -16,23 +30,99 @@ function clearError(el){
     el.innerHTML = ""
 }
 
-function validateForm(){
-    return false
+
+
+function validateName(){
+    let value = nameInput.value.trim()
+    if (value.length < 2){
+        displayError(nameError, "Minimum 2 letters")
+        return false
+    }
+    clearError(nameError)
+    return true
+}
+
+function validateEmail(){
+    let value = emailInput.value.trim()
+    if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value))){
+        displayError(emailError, "Enter a valid mail")
+        return false
+    }
+    clearError(emailError)
+    return true
+}
+
+function validateNumber(){
+    let value = numberInput.value.trim()
+    let regrex = /^[0-9\-() +]{1,20}$/
+
+    if (!regrex.test(value)){
+        displayError(numberError, "Enter a valid phonenumber, max 20 tokens")
+        return false
+    }
+    clearError(numberError)
+    return true
+}
+
+function validateStreet(){
+    let value = streetInput.value.trim()
+    if (value.length < 2 ){
+        displayError(streetError, "Minimum 2 letters")
+        return false
+    } else if( value.length > 50){
+        displayError(streetError, "Max 50 letters")
+        return false
+    }else{
+    clearError(streetError)
+    return true 
+}
+}
+
+function validatePostnumber(){
+    let value = postnumberInput.value.trim()
+  
+
+    if (value.length > 5 || value.length < 5){
+        displayError(postnumberError, "Enter a valid postnumber, 5 tokens")
+        return false
+    }
+    clearError(postnumberError)
+    return true
+}
+
+function validateCity(){
+    let value = cityInput.value.trim()
+    if (value.length < 2 ){
+        displayError(cityError, "Minimum 2 letters")
+        return false
+    } else if( value.length > 50){
+        displayError(cityError, "Max 20 letters")
+        return false}
+        else{
+    clearError(cityError)
+    return true}
+}
+
+function validateForm (){
+    let okName = validateName()
+    let okEmail = validateEmail()
+    let okNumber = validateNumber()
+    let okStreet = validateStreet()
+    let okPostnumber = validatePostnumber()
+    let okCity = validateCity()
+    return okName && okEmail && okNumber && okStreet && okPostnumber && okCity
+}
+
+if (validateForm()){
+    result.innerHTML= "Your order will be sent shortly"
+    result.className = "text-success"
+}else {
+    result.innerHTML = "fix errors"
+    result.className ="text-danger"
 }
 
 form.addEventListener("submit", function(event) {event.preventDefault()
 
 result.innerHTML=""
 
-if (validateForm()){
-    result.innerHTML= "Your order will be sent shortly"
-    result.className = "ok"
-}else {
-    result.innerHTML = "fix errors"
-    result.className ="error"
-}
-} )
-
-function validateName(){
-    let
-}
+})
