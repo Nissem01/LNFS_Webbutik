1481426e178951aa674e97daca678b8434ebd508
Linda Winther <Linda.Winther@outlook.com>
2026-04-06 11:06:21 +0200

    minor formating
    Co-authored-by: lstina, Nissem01, walterfilip

--- fetchProduct.js
+++ fetchProduct.js
@@ -1,33 +1,32 @@
-let container = document.getElementById('specificProductDisplay');
-const params  = new URLSearchParams(window.location.search);
-const productId  = params.get("id");
-console.log(productId)
+let container = document.getElementById("specificProductDisplay");
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("id");
+console.log(productId);
 
 function getProductById(productId) {
-    let APIlink = `https://fakestoreapi.com/products/${productId}`;
+  let APIlink = `https://fakestoreapi.com/products/${productId}`;
 
-    container.innerHTML = "";
+  container.innerHTML = "";
 
-    fetch(APIlink)
-        .then(response => response.json())
-        .then(product => {
-
-                const cardHTML = `
-                    <div class="col-sm-12 col-md-6 col-lg-3">
-                            <div class="card h-100 shadow-sm border-0 d-flex flex-column text-center">
-                              <img src="${product.image}" class="card-img-top p-3" style="height:180px; object-fit: contain;">
-                                <div class="card-body d-flex flex-column">
-                                    <p class="card-title">${product.title}</p>
-                                    ${product.price} €                                
+  fetch(APIlink)
+    .then((response) => response.json())
+    .then((product) => {
+      const cardHTML = `
+            
+                              <img src="${product.image}" class="img-fluid p-3" style="max-height:180px; object-fit: contain;">
+                               
+                                    <h5>${product.title}</h5>
+                                    
+                                                              
                                     <br>
                                 </div>
-                            </div>
+                            <p class="card-text">${product.description}</p> 
+                            <p class="fw-bold mb-2">${product.price} € </p> 
                     </div>
-                `
+                `;
 
-                container.innerHTML += cardHTML;
-            ;
-        })
-        .catch(error => console.error('Error:', error));
+      container.innerHTML += cardHTML;
+    })
+    .catch((error) => console.error("Error:", error));
 }
 getProductById(productId);
--- form.js
+++ form.js
@@ -1,5 +1,3 @@
-
-
 const form = document.getElementById("order");
 const nameInput = document.getElementById("name");
 const nameError = document.getElementById("nameError");
@@ -28,106 +26,112 @@ streetInput.addEventListener("blur", validateStreet);
 zipcodeInput.addEventListener("blur", validateZipcode);
 cityInput.addEventListener("blur", validateCity);
 
-
-function displayError (el, message) {
-    el.innerHTML = message
+function displayError(el, message) {
+  el.innerHTML = message;
 }
 
-function clearError(el){
-    el.innerHTML = ""
+function clearError(el) {
+  el.innerHTML = "";
 }
 
-
-
-function validateName(){
-    let value = nameInput.value.trim()
-    if (value.length < 2){
-        displayError(nameError, "Minimum 2 letters")
-        return false
-    }
-    clearError(nameError)
-    return true
+function validateName() {
+  let value = nameInput.value.trim();
+  if (value.length < 2) {
+    displayError(nameError, "Minimum 2 letters");
+    return false;
+  }
+  clearError(nameError);
+  return true;
 }
 
-function validateEmail(){
-    let value = emailInput.value.trim()
-    const check = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+function validateEmail() {
+  let value = emailInput.value.trim();
+  const check = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!check.test(value)||value.length > 50){
-        displayError(emailError, "Enter a valid mail")
-        return false
-    }
-    clearError(emailError)
-    return true
+  if (!check.test(value) || value.length > 50) {
+    displayError(emailError, "Enter a valid mail");
+    return false;
+  }
+  clearError(emailError);
+  return true;
 }
 
-function validateNumber(){
-    let value = numberInput.value.trim()
-    const check = /^[0-9\-()+]{1,20}$/
+function validateNumber() {
+  let value = numberInput.value.trim();
+  const check = /^[0-9\-()+]{1,20}$/;
 
-    if (!check.test(value)){
-        displayError(numberError, "Enter a valid phonenumber, max 20 tokens")
-        return false
-    }
-    clearError(numberError)
-    return true
+  if (!check.test(value)) {
+    displayError(numberError, "Enter a valid phonenumber, max 20 tokens");
+    return false;
+  }
+  clearError(numberError);
+  return true;
 }
 
-function validateStreet(){
-    let value = streetInput.value.trim()
-    if (value.length < 2 || value.length > 50){
-        displayError(streetError, "Minimum 2 letters")
-        return false
-    }else{
-    clearError(streetError)
-    return true 
-}
-}
-
-function validateZipcode(){
-    let value = zipcodeInput.value.trim()
-    const check = /^\d{5}$/
-
-    if (!check.test(value)){
-        displayError(zipcodeError, "Enter a valid postnumber, 5 tokens")
-        return false
-    }
-    clearError(zipcodeError)
-    return true
+function validateStreet() {
+  let value = streetInput.value.trim();
+  if (value.length < 2 || value.length > 50) {
+    displayError(streetError, "Minimum 2 letters");
+    return false;
+  } else {
+    clearError(streetError);
+    return true;
+  }
 }
 
-function validateCity(){
-    let value = cityInput.value.trim()
-    if (value.length < 2 || value.length > 20){
-        displayError(cityError, "Minimum 2 letters")
-        return false
-    } else{
-    clearError(cityError)
-    return true}
+function validateZipcode() {
+  let value = zipcodeInput.value.trim();
+  const check = /^\d{5}$/;
+
+  if (!check.test(value)) {
+    displayError(zipcodeError, "Enter a valid postnumber, 5 tokens");
+    return false;
+  }
+  clearError(zipcodeError);
+  return true;
 }
 
-function validateForm (){
-    let okName = validateName()
-    let okEmail = validateEmail()
-    let okNumber = validateNumber()
-    let okStreet = validateStreet()
-    let okZipcode = validateZipcode()
-    let okCity = validateCity()
-    return okName && okEmail && okNumber && okStreet && okZipcode && okCity
+function validateCity() {
+  let value = cityInput.value.trim();
+  if (value.length < 2 || value.length > 20) {
+    displayError(cityError, "Minimum 2 letters");
+    return false;
+  } else {
+    clearError(cityError);
+    return true;
+  }
 }
 
+function validateForm() {
+  let okName = validateName();
+  let okEmail = validateEmail();
+  let okNumber = validateNumber();
+  let okStreet = validateStreet();
+  let okZipcode = validateZipcode();
+  let okCity = validateCity();
+  return okName && okEmail && okNumber && okStreet && okZipcode && okCity;
+}
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  result.innerHTML = "";
 
-form.addEventListener("submit", function(event){
-    event.preventDefault()
+  if (validateForm()) {
+    setTimeout(() => {
+      window.location.href = "receipt.html";
+    }, 1500);
+  } else {
+    result.innerHTML = "fix errors";
+    result.className = "text-danger";
+  }
+});
 
-result.innerHTML=""
-
-if (validateForm()){
-    result.innerHTML= "Your order will be sent shortly"
-    result.className = "text-success"
-}else {
-    result.innerHTML = "fix errors"
-    result.className ="text-danger"
-}})
+const checkbox = document.getElementById("newsletter");
+checkbox.addEventListener("change", function () {
+  if (!checkbox.checked) {
+    setTimeout(() => {
+      checkbox.checked = true;
+    }, 1000);
+  }
+});
